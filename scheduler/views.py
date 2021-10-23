78024e7d5ee501517e9e6b8ce466f431d8ec04ea
MicroTot <32261713+MicroTot@users.noreply.github.com>
2021-10-24 01:19:17 +0300

    heroku

--- scheduler/views.py
+++ scheduler/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from scheduler.models import Appointment
 from scheduler.serializers import AppointmentSerializers
 
-# Create your views here.
+# Create your views here
 class AppointmentDetails(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializers
